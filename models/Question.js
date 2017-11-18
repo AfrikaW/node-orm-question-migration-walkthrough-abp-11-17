7d501b8e65daca090cf3910dc6d85ca10ee7f6d9
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-18 20:18:08 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -5,9 +5,13 @@ const db = require("../config/db")
 class Question{
   static CreateTable() {
     return new Promise(function(resolve){
-      resolve("This Does Nothing!")
+      const sql = `CREATE TABLE questions (id INTEGER PRIMARY KEY)`
+      db.run(sql, function(){
+        resolve("questions table created")
+      })      
     })
   }
 }
 
 module.exports = Question;
+
